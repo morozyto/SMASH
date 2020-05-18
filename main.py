88b5c78bdfe86a68ee942e1bd694a0dae3104bd5
Anton Morozov <moranton@mail.ru>
2020-05-19 00:36:56 +0300

    move some things in modules; more logs

--- main.py
+++ main.py
@@ -4,36 +4,14 @@ import random
 import hss
 import log
 import tools
-
-
-def k(x, y):
-    """
-    Kernel function.
-    :param x: first_arg
-    :param y: second_arg
-    :rtype: int
-    """
-    dx = 1
-    if (x != y):
-        return 1 / (x - y)
-    else:
-        return dx
+import taylor_expansion
 
 if __name__ == "__main__":
+    np.set_printoptions(precision=3)
 
     tolerance = 10 ** -7
     dimension_count = 1
     tools.count_constants(dimension_count, tolerance)
-
-    def get_uniform_values():
-        start_value = 0
-        n = 50 # 51
-
-        step = 1 / n
-        assert step > 0
-        assert n > 0
-        obj = [i for i in np.arange(start_value, start_value + n * step, step)]
-        return obj, obj
 
     def get_cauchy_values():
         n = 51
@@ -41,17 +19,19 @@ if __name__ == "__main__":
         y = [x_ + (10 ** -7) * random.random() for x_ in x]
         return x, y
 
-    x_values, y_values = get_uniform_values()
+    x_values, y_values = tools.get_uniform_values(start_value=0, end_value=1, n=1000) # 50, 51
 
     log.info('Starting HSS')
 
-    A = np.array([np.array([k(x, y) for y in y_values]) for x in x_values])
+    A = np.array([np.array([taylor_expansion.k(x, y) for y in y_values]) for x in x_values])
 
     log.debug(f'X index values is {x_values}')
     log.debug(f'Y index values is {y_values}')
-    log.debug(f'Not compressed A is {A}')
+    log.debug(f'Not compressed A is \n{A}')
 
-    A_ = hss.HSS(x_values, y_values, A)
+    max_values_in_node = 30
+
+    A_ = hss.HSS(x_values, y_values, A, max_values_in_node)
     log.debug(f'Printing result HSS\n{A_}')
 
     vec = np.array([1] * A.shape[1])
@@ -66,11 +46,12 @@ if __name__ == "__main__":
     log.info(f'Compressed result:\n{compr_result}')
     log.info(f'Error vec norm: {error}')
     log.info(f'Relative error: {error / np.linalg.norm(not_compr_result)}')
-
+    '''
     log.info(f'Going to solve system with b {vec}')
 
     not_compr_result = np.linalg.solve(A, vec)
     compr_result = A_.solve(vec)
+    log.debug(f'Not compressed A is \n{A}')
     error_vec = not_compr_result - compr_result
     error = np.linalg.norm(error_vec)
 
@@ -79,3 +60,4 @@ if __name__ == "__main__":
     log.info(f'Error vec norm: {error}')
     log.info(f'Relative error: {error / np.linalg.norm(not_compr_result)}')
     #assert error < 3
+    '''
