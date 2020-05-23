5205654c5874d125717bcfc9732b0baf9b40011b
Anton Morozov <moranton@mail.ru>
2020-05-23 20:19:00 +0300

    add usefull methods

--- log.py
+++ log.py
@@ -1,21 +1,27 @@
 import logging
 from time import gmtime, strftime
+import os
+__all__ = ['debug', 'info', 'warning', 'error', 'critical', 'is_debug']
 
-__all__ = ['debug', 'info', 'warning', 'error', 'critical']
-
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 SAVE_FILE_LOG = True
 
 logger = logging.getLogger("HSS")
 
-time_postfix = strftime("%d_%m_%Y,%H:%M:%S", gmtime())
+time_postfix = 'log' ##strftime("%d_%m_%Y,%H:%M:%S", gmtime())
 logs_name = 'logs/' + time_postfix + '.log'
+
+if os.path.exists(logs_name):
+    os.remove(logs_name)
 
 fh = logging.FileHandler(logs_name) if SAVE_FILE_LOG else logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.setLevel(LOG_LEVEL)
+
+def is_debug():
+    return LOG_LEVEL != logging.INFO
 
 
 def critical(msg):
